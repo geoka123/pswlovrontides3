2dddf0e99bb1be393d1bc98744188489a179c40c
o malakas <geokaraviotis@gmail.com>
2024-10-22 22:55:16 +0300

    Kalyteri version join kai detach

--- kernel_threads.c
+++ kernel_threads.c
@@ -29,26 +29,48 @@ Tid_t sys_CreateThread(Task task, int argl, void* args)
 /**
   @brief Return the Tid of the current thread.
  */
-PTCB* sys_ThreadSelf()
+Tid_t sys_ThreadSelf()
 {
-	return (PTCB*) ((Tid_t) cur_thread()); // Return PTCB better
+	return (Tid_t) cur_thread()->ptcb ; // Return PTCB better
 }
 
 /**
   @brief Join the given thread.
   */
-int sys_ThreadJoin(Tid_t tid, int* exitval) // Mounakia tin analamvanei o JETT aythn
+int sys_ThreadJoin(Tid_t tid, int* exitval) //
 {
   // ----- ELEGXOI POU PREPEI NA GINOUN EDO -----
-  // 1) An to dosmeno tid den yparxei
+  // 1) An to dosmeno tid den yparxei  apo listapcb arrlist_find
   // 2) An to tid tis parametrou einai to tid tou thread poy kalei thn join
   // 3) An to tid poy dinetai anikei se thread poy exei kanei detach
   // 4) Ta 2 threads (auto poy kalei kai ayto pou kaleitai) na anikoun sto idio PCB
 
-  PTCB* cur_thread_ptcb = sys_ThreadSelf();
+  PTCB* cur_thread_ptcb = (PTCB*) sys_ThreadSelf();
+  PTCB* thread_to_join_in = (PTCB*) tid;
+  PCB* parent_pcb = cur_thread_ptcb->tcb->owner_pcb;
+
+  // ----- TESTS INIT -----
+  if (rlist_find(&(parent_pcb->ptcb_list), &(cur_thread_ptcb->ptcb_list_node), NULL) == NULL)
+    return -1;
   if (cur_thread_ptcb == ((PTCB*) tid) || (cur_thread_ptcb -> detached) == 1 || cur_thread_ptcb->tcb->owner_pcb == ((PTCB*) tid)->tcb->owner_pcb)
     return -1; 
-  kernel_wait(&(((PTCB*) tid) -> exit_cv), SCHED_MUTEX);  // Made current thread get into waiting list of the thread that exists as a parameter
+  
+  // ----- TESTS PASSED -----
+  cur_thread_ptcb->refcount++; // refcount++
+
+  while (cur_thread_ptcb->exited == 0 && cur_thread_ptcb->detached == 0)      //while thread not detached and not exited
+    kernel_wait(&(((PTCB*) tid) -> exit_cv), SCHED_USER);    // Made current thread get into waiting list of the thread that exists as a parameter
+  
+  if (thread_to_join_in->detached == 1) //return -1 if t2 was detached
+    return -1;
+  
+  if (exitval != NULL)    // if exit val is not null *exit_val == ptcb->*exit_val
+    cur_thread_ptcb->exitval == &exitval;
+  
+  cur_thread_ptcb->refcount--;    // refcount--
+  if (cur_thread_ptcb->refcount == 0)      // if refcount == 0 then remove ptcb from curproc
+    rlist_remove(cur_thread_ptcb);
+
 	return 0;
 }
 
@@ -57,11 +79,24 @@ int sys_ThreadJoin(Tid_t tid, int* exitval) // Mounakia tin analamvanei o JETT a
   */
 int sys_ThreadDetach(Tid_t tid)
 {
-  PTCB* cur_thread_ptcb = (PTCB*) tid;
-  if (cur_thread_ptcb->detached == 0)
-    cur_thread_ptcb->detached == 1;
-    return 0;
-	return -1;
+
+  // kanw ta testakia poy grafei sto brief toy tinyos.h
+  // prosexe gia tin rlist_find
+  // ama perasoun ola ta testakia kane broadcast
+  
+  PTCB* ptcb_of_thread = (PTCB*) tid;
+  PCB* parent_pcb = ptcb_of_thread->tcb->owner_pcb;
+
+  if (ptcb_of_thread->exited == 1)   // Check if the given tid corresponds to an exited thread
+    return -1;
+  
+  if (rlist_find(&(parent_pcb->ptcb_list), &(ptcb_of_thread->ptcb_list_node), NULL) == NULL)    // Checking if the given tid actually exists
+    return -1;
+
+  ptcb_of_thread->detached == 1;    // Making the tid detached
+  kernel_broadcast(&(ptcb_of_thread->exit_cv));    // Waking up all the waiting threads and telling them that they aint gonna join it
+
+	return 0;
 }
 
 /**
