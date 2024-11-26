b3e5f5f5db1ba43918d8d4fffa64b5a37d515ea8
o malakas <geokaraviotis@gmail.com>
2024-11-26 10:25:17 +0200

    Cleared errors and malakies from sys_pipe

--- kernel_pipe.c
+++ kernel_pipe.c
@@ -5,14 +5,24 @@
 #include "kernel_proc.h"
 #include "kernel_dev.h"
 
-static file_ops reader_file_ops;
-reader_file_ops->Open = invalidFunction2(); //thelei asteraki sto Open??????den exo idea
-reader_file_ops->Write = invalidFunction1();
+int invalidFunction1(){
+	return -1;
+}
+	
+void invalidFunction2(){
+	return NULL;
+}
+
+static file_ops reader_file_ops = {
+	.Open = invalidFunction1,
+	.Write = invalidFunction1
+};
 
 
-static file_ops writer_file_ops;
-writer_file_ops->Open = invalidFunction2();
-writer_file_ops->Read= invalidFunction1();
+static file_ops writer_file_ops = {
+	.Open = invalidFunction1,
+	.Read = invalidFunction2
+};
 
 
 
@@ -39,49 +49,40 @@ int sys_Pipe(pipe_t* pipe)
 
 
 		get_fcb(read)->streamobj = ppcb;
-		get_fcb(read)->streamfunc = writer_file_ops;
+		get_fcb(read)->streamfunc = &writer_file_ops;
 
 
 
 		get_fcb(write)->streamobj = ppcb;
-		get_fcb(read)->streamfunc = reader_file_ops;
+		get_fcb(read)->streamfunc = &reader_file_ops;
 
-		reader_file_ops->Read = pipe_read(ppcb, ppcb->buffer, n!!!);
+		reader_file_ops.Read = pipe_read(ppcb, ppcb->buffer, PIPE_BUFFER_SIZE);
 
-		writer_file_ops->Write = pipe_write(ppcb , ppcb->buffer , n!!!);
+		writer_file_ops.Write = pipe_write(ppcb , ppcb->buffer , PIPE_BUFFER_SIZE);
 
-		reader_file_ops->Close = pipe_reader_close(ppcb);
+		reader_file_ops.Close = pipe_reader_close(ppcb);
 
-		writer_file_ops->Close = pipe_writer_close(ppcb);
+		writer_file_ops.Close = pipe_writer_close(ppcb);
 
 		return 0;
 	}
+	else
+		return -1;
+}
+
+int pipe_write(void* ppcb , const char *buf , unsigned int n){// den exo katalabei ayto to N ti rolo paizei . An prokeitai gia to size toy buffer tote mallon tha prepei na ginei define edw kai oxi sthn kernel_pipe.h
 	return -1;
+}
 
+int pipe_read(void* ppcb ,char *buf , unsigned int n){
+	return -1;
+}
 
-	int invalidFunction1(){
-		return -1;
-	}
-	void invalidFunction2(){
-		return NULL;
-	}
+int pipe_writer_close(void* ppcb){
+	return -1;
+}
 
-
-
-	int pipe_write(void* ppcb , const char *buf , unsigned int n){// den exo katalabei ayto to N ti rolo paizei . An prokeitai gia to size toy buffer tote mallon tha prepei na ginei define edw kai oxi sthn kernel_pipe.h
-		return -1
-	}
-
-	int pipe_read(void* ppcb ,char *buf , unsigned int n){
-		return -1
-	}
-
-	int pipe_writer_close(void* ppcb){
-		return -1;
-	}
-
-	int pipe_reader_close(void* ppcb){
-		return -1;
-	}
+int pipe_reader_close(void* ppcb){
+	return -1;
 }
 
