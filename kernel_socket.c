2567d1f6e76108be8e539aed68f37cec7e9a292c
charge9 <giorgosch405@gmail.com>
2024-12-11 13:24:51 +0200

    sockets5

--- kernel_socket.c
+++ kernel_socket.c
@@ -2,8 +2,8 @@
 #include "kernel_socket.h"
 #include "kernel_streams.h"
 #include "kernel_proc.h"
- 
-#include "kernel_dev.h"
+
+//#include "kernel_dev.h"
 #include "kernel_cc.h" 
 
 SCCB* PORT_MAP[MAX_PORT];
@@ -42,33 +42,40 @@ int socket_write(void* sccb , const char *buf , unsigned int n){
 	}
 	return -1;
 }
-
 int socket_close(void* sccb){
-	Fid_t sock = (Fid_t)(intptr_t) sccb;
-	FCB* fcb = get_fcb(sock);
-
-	if(fcb==NULL){
-		return -1;
+	if(sccb!= NULL){
+		SCCB* my_sccb = (SCCB*)sccb;
+		
+		FCB* my_fcb = my_sccb->fcb;
+		if(my_fcb==NULL){
+			return -1;
+		}
+		//SCCB* my_sock =(SCCB*)my_fcb->streamobj;
+		for(int i = 0;i< MAX_FILEID; i++){
+			if(CURPROC->FIDT[i] == my_fcb){
+				Fid_t fid = (Fid_t)i;
+				if(sys_ShutDown(fid,SHUTDOWN_BOTH)!=0){
+					return -1;
+				}
+			}
+		}
+		if(my_sccb->refcount == 0){
+			PORT_MAP[my_sccb->port]=NULL;
+			//my_fcb->streamobj = NULL;
+			my_sccb = NULL;
+			free(my_sccb);
+			return -1;
+		}
+		return 0;
 	}
-
-	if(sys_ShutDown(sock,SHUTDOWN_BOTH)!=0){
-		return -1;
-	}
-
-	SCCB* my_sock =(SCCB*)fcb->streamobj;
-
-	if(my_sock->refcount == 0){
-		my_sock = NULL;
-		free(my_sock);
-	}
-	return 0;
+	return -1;
 }
 
 static file_ops socket_file_ops = {
-  .Open = socket_invalidFunction_open,
-  .Read = socket_read,
-  .Write = socket_write,
-  .Close = socket_close,
+	.Open = socket_invalidFunction_open,
+	.Read = socket_read,
+	.Write = socket_write,
+	.Close = socket_close
 };
 
 
@@ -76,13 +83,13 @@ Fid_t sys_Socket(port_t port)
 {
 	Fid_t fid;
 	FCB* fcb;
-	if(port>=NOPORT && port <= MAX_PORT){	
+	if((port>=NOPORT && port <= MAX_PORT)){
 		if(FCB_reserve(1,&fid,&fcb)){
 			SCCB* sccb;
-			sccb = xmalloc(sizeof(SCCB));
+			sccb = (SCCB*)xmalloc(sizeof(SCCB));
 			sccb->refcount = 0;
 			sccb->type = SOCKET_UNBOUND; //unbound
-			if(port!=NOPORT)
+			if(port>NOPORT)
 				sccb->port = port;
 			else
 				sccb->port = NOPORT;
@@ -96,7 +103,7 @@ Fid_t sys_Socket(port_t port)
 
 int sys_Listen(Fid_t sock)
 {
-	if(sock ==NOFILE)
+	if(sock == NOFILE)
 		return -1;
 
 	FCB* fcb_sock = get_fcb(sock);
@@ -108,7 +115,7 @@ int sys_Listen(Fid_t sock)
 	if (my_sock == NULL || my_sock->port == NOPORT)
 		return -1;
 
-	if (PORT_MAP[my_sock->port] != NULL && PORT_MAP[my_sock->port]->type == SOCKET_LISTENER)
+	if (PORT_MAP[my_sock->port] != NULL)
 		return -1;
 
 	// Install the socket into PORT_MAP[]
@@ -126,7 +133,7 @@ int sys_Listen(Fid_t sock)
 Fid_t sys_Accept(Fid_t lsock)
 {
 	// ---------- ELEGXOI ----------
-	if(lsock ==NOFILE)
+	if(lsock == NOFILE)
 		return -1;
 	
 	FCB* fcb_of_sock = get_fcb(lsock);
@@ -160,12 +167,12 @@ Fid_t sys_Accept(Fid_t lsock)
 	connection_request* first_request = (connection_request*) rlist_pop_front(&my_sock->listener_s.queue);
 	if(first_request == NULL)
 		return NOFILE;
-	first_request->admitted = 1;
+	
 
 	// Try to construct peer    APO POU PAIRNO TA PPCB KAI AN TO KANO SOSTA
 	//my_sock->type = SOCKET_PEER;
 	SCCB* my_sock2 = first_request->peer; //s2
-	peer_socket* peer_of_sock2 = &my_sock2->peer_s;
+	peer_socket* peer_of_sock2 = (peer_socket*)my_sock2;
 	
 	
 	Fid_t my_sock_fid3 = sys_Socket(my_sock->port); //s3 ///edo ti ginetai charge9
@@ -179,12 +186,14 @@ Fid_t sys_Accept(Fid_t lsock)
 	if(my_sock3 == NULL)
 		return NOFILE;
 	my_sock3->type = SOCKET_PEER;
-	peer_socket* peer_of_sock3 = &my_sock3->peer_s;
+	peer_socket* peer_of_sock3 = (peer_socket*)my_sock3;
 
 	// Ftiaxno PPCB* me xmalloc
 	PPCB* pipe1 = xmalloc(sizeof(PPCB));
 	PPCB* pipe2 = xmalloc(sizeof(PPCB));
 	
+	//pipe initialization 
+
 	// Ftiaxno ta peer to peer
 	peer_of_sock2->read_pipe = pipe1;
 	peer_of_sock3->write_pipe = pipe1;
@@ -194,7 +203,7 @@ Fid_t sys_Accept(Fid_t lsock)
 
 	peer_of_sock2->peer = my_sock3;
 	peer_of_sock3->peer = my_sock2;
-	
+	first_request->admitted = 1;
 	kernel_signal(&(first_request->connected_cv));
 	my_sock->refcount--;
 
@@ -207,6 +216,9 @@ int sys_Connect(Fid_t sock, port_t port, timeout_t timeout)
 	// 	return -1;
 	// }
 
+	if(sock == NOFILE){
+		retrun -1;
+	}
 	FCB * my_sock = get_fcb(sock);
 
 	if(my_sock->streamobj==NULL){
@@ -222,6 +234,7 @@ int sys_Connect(Fid_t sock, port_t port, timeout_t timeout)
 	if(PORT_MAP[port]==NULL){
 		return -1;
 	}
+
 	SCCB* is_listener = PORT_MAP[port];//s1 listener
 
 	if(!(is_listener->type == SOCKET_LISTENER)){
@@ -229,11 +242,9 @@ int sys_Connect(Fid_t sock, port_t port, timeout_t timeout)
 	}
 
 	is_listener->refcount++;
+	sccb->type = SOCKET_PEER;
 
 	//sccb->type = SOCKET_PEER;//charge 
-	if(sccb->type != SOCKET_UNBOUND){
-		return -1;
-	}
 	connection_request* con_req = (connection_request*)xmalloc(sizeof(connection_request));
 
 	con_req -> admitted = 0;
@@ -260,41 +271,80 @@ int sys_Connect(Fid_t sock, port_t port, timeout_t timeout)
 
 int sys_ShutDown(Fid_t sock, shutdown_mode how)
 {
-	if(sock >= 0 && sock <= MAX_FILEID -1){
-		FCB* my_sock = (FCB*)&sock;
-		if(my_sock->streamobj!= NULL){
-			SCCB* sccb = (SCCB*)my_sock->streamobj;
-			if(sccb->type == SOCKET_PEER){
-				switch(how){
-					case SHUTDOWN_READ:
-						peer_socket* peer = &sccb->peer_s;
-						return pipe_reader_close(peer->read_pipe);
-					break;
+	// if(sock >= 0 && sock <= MAX_FILEID -1){
+	// 	FCB* my_sock = (FCB*)&sock;
+	// 	if(my_sock->streamobj!= NULL){
+	// 		SCCB* sccb = (SCCB*)my_sock->streamobj;
+	// 		if(sccb->type == SOCKET_PEER){
+	// 			switch(how){
+	// 				case SHUTDOWN_READ:
+	// 					peer_socket* peer = &sccb->peer_s;
+	// 					return pipe_reader_close(peer->read_pipe);
+	// 				break;
 
-					case SHUTDOWN_WRITE:
-						peer_socket* peer2 = &sccb->peer_s;
-						return pipe_writer_close(peer2->write_pipe);
-					break;
+	// 				case SHUTDOWN_WRITE:
+	// 					peer_socket* peer2 = &sccb->peer_s;
+	// 					return pipe_writer_close(peer2->write_pipe);
+	// 				break;
 
-					case SHUTDOWN_BOTH:
-						peer_socket* peer3 = &sccb->peer_s;
-						int j = pipe_writer_close(peer3->write_pipe);
-						int i = pipe_reader_close(peer3->read_pipe);
-						if(i==j){
-							return i;
-						}
-						return -1;
-					break;
+	// 				case SHUTDOWN_BOTH:
+	// 					peer_socket* peer3 = &sccb->peer_s;
+	// 					int j = pipe_writer_close(peer3->write_pipe);
+	// 					int i = pipe_reader_close(peer3->read_pipe);
+	// 					if(i==j){
+	// 						return i;
+	// 					}
+	// 					return -1;
+	// 				break;
 
-					default:
-						return -1;
-				}
-				//return 0;
-			}
-			return -1;
-		}
+	// 				default:
+	// 					return -1;
+	// 			}
+	// 			//return 0;
+	// 		}
+	// 		return -1;
+	// 	}
+	// 	return -1;
+	// }
+	// return -1;
+	if(!(sock >= 0 && sock <= MAX_FILEID -1)){
 		return -1;
 	}
-	return -1;
+
+	FCB* my_sock = get_fcb(sock);
+	if(my_sock->streamobj== NULL){
+		return -1;
+	}
+	SCCB* sccb = (SCCB*)my_sock->streamobj;
+	if(sccb == NULL)
+		return -1;
+	if(sccb->type != SOCKET_PEER){
+		return -1;
+	}
+	switch(how){
+		case SHUTDOWN_READ:
+			peer_socket* peer = &sccb->peer_s;
+			return pipe_reader_close(peer->read_pipe);
+		break;
+
+		case SHUTDOWN_WRITE:
+			peer_socket* peer2 = &sccb->peer_s;
+			return pipe_writer_close(peer2->write_pipe);
+		break;
+
+		case SHUTDOWN_BOTH:
+			peer_socket* peer3 = &sccb->peer_s;
+			int j = pipe_writer_close(peer3->write_pipe);
+			int i = pipe_reader_close(peer3->read_pipe);
+			if(i==j){
+				return i;
+			}
+			return -1;
+		break;
+
+		default:
+			return -1;
+	}
+
 }
 
